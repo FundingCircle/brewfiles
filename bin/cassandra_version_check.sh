3d4ce8dad0d0d0eb1abbbd4baf9ec53213bd15ce
Sasha Gerrand <sasha.gerrand@fundingcircle.com>
2015-10-15 22:30:30 +0100

    Fail fast

--- bin/cassandra_version_check.sh
+++ bin/cassandra_version_check.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 if brew list -1 | grep -q "^cassandra$"; then
   echo "Warning: You are using the latest version of Cassandra, which is currently incompatible with CODAS."
   read -p "Would you like to remove it and install version 1.2? [y/N] " prompt
