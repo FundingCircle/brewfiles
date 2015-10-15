0de0b4d9b998ae26ff0ef3fa9c62bb862269ba6d
Miroslav Csonka <miroslav.csonka@gmail.com>
2015-10-15 14:00:04 +0100

    Check for correct version of Cassandra

---
+++ bin/cassandra_version_check.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if brew list -1 | grep -q "^cassandra$"; then
+  echo "Warning: You are using the latest version of Cassandra, which is currently incompatible with CODAS."
+  read -p "Would you like to remove it and install version 1.2? [y/N] " prompt
+  if [[ $prompt == "y" || $prompt == "Y" ]]; then
+    brew uninstall cassandra
+    rm -rf /usr/local/var/lib/cassandra
+    rm -rf /usr/local/etc/cassandra
+  fi
+fi
